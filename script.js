362fb6a8bba62c918fe97116a960ed966d458fd4
RoshanSyangtanTamang <rossonsyangtan444@gmail.com>
2025-03-02 16:55:38 +0545

    Adding JS

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ let CompScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    const rdmIdx = Math.floor(Math.random() * 3);
+    return options[rdmIdx];
+}
+
+const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
+    const compChoice = genCompChoice();
+    console.log("comp choice = ", compChoice);
+}
+
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id")
-        console.log("choice was clicked", userChoice);
+        playGame(userChoice);
     })
 })
